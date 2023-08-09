672b4e7a49efefe58c652c82977f8338c7dee920
devseag <example@gmail.com>
2023-08-09 20:22:17 -0300

    add buffered read file 404

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,26 +1,41 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 
 import static java.awt.SystemColor.text;
 
+//public class ReadFile {
+//    public static void main(String[] args) {
+//        try (FileInputStream in = new FileInputStream("data/input.txt")) {
+//            StringBuilder text = new StringBuilder();
+//            int read;
+//            while ((read = in.read()) != -1) {
+//                text.append((char) read);
+//            }
+//            String[] lines = text.toString().split(System.lineSeparator());
+//            for (String line : lines) {
+//                System.out.println(line);
+//            }
+////            System.out.println(text);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+//}
+
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("data/input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
-            }
-//            System.out.println(text);
+        try (BufferedReader in = new BufferedReader(new FileReader("data/input.txt"))) {
+//            for (String line = in.readLine(); line != null; line = in.readLine()) {
+//                System.out.println(line);
+//            }
+            in.lines().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
