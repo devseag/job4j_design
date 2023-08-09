7869d6c2d8ef4ab4e317e426965074546e0dd999
devseag <example@gmail.com>
2023-08-09 16:51:42 -0300

    add read even file

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            Arrays.stream(text.toString().split(System.lineSeparator()))
+                    .filter(s -> Integer.parseInt(s) % 2 == 0)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+import static java.awt.SystemColor.text;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("data/input.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                System.out.println(line);
+            }
+//            System.out.println(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
