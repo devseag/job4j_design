05a411d76c59cb2bab7efdd746b39b0f631be350
devseag <example@gmail.com>
2023-08-10 16:48:28 -0300

    add validation

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -13,8 +13,30 @@ public class Search {
 //        Files.walkFileTree(start, new PrintFiles());
 //    }
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+//        Path start = Paths.get(".");
+//        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+//    }
+        new Search().validation(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile()
+                .getName()
+                .endsWith(args[1]))
+                .forEach(System.out::println);
+    }
+
+    public void validation(String[] arg) {
+        if (arg.length != 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar ROOT_FOLDER.");
+        }
+        if (!Paths.get(arg[0]).toFile().exists()) {
+            throw new IllegalArgumentException("File does not exit.");
+        }
+        if (!Paths.get(arg[0]).toFile().isDirectory()) {
+            throw new IllegalArgumentException("Not a directory.");
+        }
+        if (!arg[1].startsWith(".")) {
+            throw new IllegalArgumentException("Not a directory.");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
