99631de11049e5a0265be6cd1b3261309880d187
devseag <example@gmail.com>
2023-08-09 21:29:11 -0300

    add buffered output stream

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,8 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -30,9 +28,22 @@ public class LogFilter {
         return rsl;
     }
 
+    public void saveTo(String out) {
+        var data = filter();
+        try (PrintWriter printWriter = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(out)
+                ))) {
+            data.forEach(printWriter::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter("data/log.txt");
-        logFilter.filter().forEach(System.out::println);
+//        LogFilter logFilter = new LogFilter("data/log.txt");
+//        logFilter.filter().forEach(System.out::println);
+        new LogFilter("data/log.txt").saveTo("data/404.txt");
 
     }
 }
--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,21 +1,40 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+//public class ResultFile {
+//    public static void main(String[] args) {
+//        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+////            out.write("Hello, world!".getBytes());
+////            out.write(System.lineSeparator().getBytes());
+//            for (int i = 1; i <= 9; i++) {
+//                for (int j = 1; j <= 9; j++) {
+//                    String rsl = i + " * " + j + " = " + i * j;
+//                    out.write(rsl.getBytes());
+//                    out.write(System.lineSeparator().getBytes());
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-//            out.write("Hello, world!".getBytes());
-//            out.write(System.lineSeparator().getBytes());
-            for (int i = 1; i <= 9; i++) {
-                for (int j = 1; j <= 9; j++) {
-                    String rsl = i + " * " + j + " = " + i * j;
-                    out.write(rsl.getBytes());
-                    out.write(System.lineSeparator().getBytes());
-                }
-            }
-        } catch (IOException e) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/result.txt")
+                ))) {
+            out.println("Hello, world!");
+            out.printf("%s%n", "Some string");
+            out.printf("%d%n", 10);
+            out.printf("%f%n", 1.5f);
+//            out.printf("%s", System.lineSeparator());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
