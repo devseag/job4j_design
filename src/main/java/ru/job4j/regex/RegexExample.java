d0a916e1d86fa9ef8dc7c3c6b25451ddc98f8a9f
devseag <example@gmail.com>
2023-08-10 22:10:38 -0300

    add regexp patterns

--- src/main/java/ru/job4j/regex/RegexExample.java
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -1,5 +1,6 @@
 package ru.job4j.regex;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,13 +39,43 @@ public class RegexExample {
 //            System.out.println("Najdeno sovpadenie. iStart: " + matcher.start()
 //                    + " iEnd: " + matcher.end());
 
-        Pattern pattern = Pattern.compile("123");
-        String text = "1231 i 1232 i 1233";
+//        Pattern pattern = Pattern.compile("123");
+//        String text = "1231 i 1232 i 1233";
+//        Matcher matcher = pattern.matcher(text);
+//        String rsl = matcher.replaceAll("Job4j");
+//        System.out.println(rsl);
+
+//        Pattern pattern = Pattern.compile("11");
+//        String text = "111111";
+//        Matcher matcher = pattern.matcher(text);
+//        while (matcher.find()) {
+//            System.out.println("Najdeno sovpadenie: " + matcher.group());
+//        }
+
+//        String str = "123+=-456:/789";
+//        String[] rsl = str.split("\\D+");
+//        System.out.println(Arrays.toString(rsl)); // [123, 456, 789]
+
+//        Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}\\.\\d{4}");
+//        Najdeno sovpadenie: 24.04.1987
+//        Najdeno sovpadenie: 11.11.1111
+//        Najdeno sovpadenie: 99.99.9999
+//        Najdeno sovpadenie: 99.99.9999
+//        Pattern pattern = Pattern.compile("\\b\\d{2}\\.\\d{2}\\.\\d{4}\\b");
+////        Najdeno sovpadenie: 24.04.1987
+////        Najdeno sovpadenie: 99.99.9999
+//        String text = "24.04.1987 11.11.111111 99.99.99991 99.99.9999 99999999 0000.00.00";
+//        Matcher matcher = pattern.matcher(text);
+//        while (matcher.find()) {
+//            System.out.println("Najdeno sovpadenie: " + matcher.group());
+//        }
+
+
+        Pattern pattern = Pattern.compile("\\S{1,}@\\S{1,}\\.\\S{1,}");
+        String text = "peter-2022@example.com example65@mail_box.ru 123_45@example-mailbox.com";
         Matcher matcher = pattern.matcher(text);
-        String rsl = matcher.replaceAll("Job4j");
-        System.out.println(rsl);
-
-
-
+        while (matcher.find()) {
+            System.out.println("Najdeno sovpadenie: " + matcher.group());
+        }
     }
 }
