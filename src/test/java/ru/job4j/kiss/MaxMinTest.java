e576001197cd3b91bf81a383eb732c7b2837e3e0
devseag <example@gmail.com>
2023-08-17 12:25:03 -0300

    add kiss

--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -2,6 +2,7 @@ package ru.job4j.kiss;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,14 +10,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MaxMinTest {
 
     @Test
-    void minTest() {
+    void whenFindMin() {
         List<Integer> list = List.of(1, 0, 2, 3, 4, 5);
         assertThat(new MaxMin().min(list, Integer::compareTo)).isEqualTo(0);
     }
 
     @Test
-    void maxTest() {
+    void whenFindMax() {
         List<Integer> list = List.of(1, 0, 2, 3, 4, 5);
         assertThat(new MaxMin().max(list, Integer::compareTo)).isEqualTo(5);
+    }
+
+    @Test
+    void whenFindMaxInEmptyListThenGetException() {
+        List<Integer> list = new ArrayList<>();
+        assertThat(new MaxMin().max(list, Integer::compareTo)).isNull();
     }
 }
