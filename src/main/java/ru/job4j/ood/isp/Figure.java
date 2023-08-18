5879248c722c978b4e440b36c64ab9f800db1c12
devseag <example@gmail.com>
2023-08-18 10:40:33 -0300

    add isp interface segregation principle

---
+++ src/main/java/ru/job4j/ood/isp/Figure.java
@@ -0,0 +1,18 @@
+package ru.job4j.ood.isp;
+
+interface Figure {
+
+    double perimetr();
+    double square();
+    double volume();
+
+}
+
+interface Figure2D {
+    double perimetr();
+    double square();
+}
+
+interface Figure3D {
+    double volume();
+}
